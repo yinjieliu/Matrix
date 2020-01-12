d17e55ba15ee7cb2b0f327487a3f97133b844b22
Yinjie <mrjie1010@gmail.com>
2020-01-11 16:23:06 -0800

    Create customized adapter for ListView

--- app/src/main/java/com/laioffer/matrix/MainActivity.java
+++ app/src/main/java/com/laioffer/matrix/MainActivity.java
@@ -3,12 +3,21 @@ package com.laioffer.matrix;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get ListView object from xml.
+        ListView eventListView = (ListView) findViewById(R.id.event_list);
+
+
+        // Assign adapter to ListView.
+        EventAdapter adapter = new EventAdapter(this);
+        eventListView.setAdapter(adapter);
     }
 }
